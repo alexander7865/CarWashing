57f6255d201d44399c88318d9c7c715cdf816541
Parkduksung <duksung1234@naver.com>
2022-04-10 10:35:11 +0900

    [4.10] hashkey 구하는 코드 추가.

--- app/src/main/java/com/mod_int/carwash/ui/map/MapActivity.kt
+++ app/src/main/java/com/mod_int/carwash/ui/map/MapActivity.kt
@@ -1,14 +1,42 @@
 package com.mod_int.carwash.ui.map
 
+import android.annotation.SuppressLint
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Base64
+import android.util.Log
 import com.mod_int.carwash.R
 import com.mod_int.carwash.base.BaseActivity
 import com.mod_int.carwash.databinding.ActivityMapBinding
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
 
 class MapActivity : BaseActivity<ActivityMapBinding>(R.layout.activity_map) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        getHashKey()
+    }
 
+    @SuppressLint("PackageManagerGetSignatures")
+    private fun getHashKey() {
+        var packageInfo: PackageInfo? = null
+        try {
+            packageInfo =
+                packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNATURES)
+        } catch (e: PackageManager.NameNotFoundException) {
+            e.printStackTrace()
+        }
+        if (packageInfo == null) Log.e("KeyHash", "KeyHash:null")
+        for (signature in packageInfo!!.signatures) {
+            try {
+                val md: MessageDigest = MessageDigest.getInstance("SHA")
+                md.update(signature.toByteArray())
+                Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT))
+            } catch (e: NoSuchAlgorithmException) {
+                Log.e("KeyHash", "Unable to get MessageDigest. signature=$signature", e)
+            }
+        }
     }
 }
