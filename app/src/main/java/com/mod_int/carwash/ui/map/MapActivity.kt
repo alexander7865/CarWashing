1d5b7ff3832fb4cd320fa40929ed19a5aae6ed3b
Parkduksung <duksung1234@naver.com>
2022-04-10 10:26:34 +0900

    [4.10] mapview init

---
+++ app/src/main/java/com/mod_int/carwash/ui/map/MapActivity.kt
@@ -0,0 +1,14 @@
+package com.mod_int.carwash.ui.map
+
+import android.os.Bundle
+import com.mod_int.carwash.R
+import com.mod_int.carwash.base.BaseActivity
+import com.mod_int.carwash.databinding.ActivityMapBinding
+
+class MapActivity : BaseActivity<ActivityMapBinding>(R.layout.activity_map) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
